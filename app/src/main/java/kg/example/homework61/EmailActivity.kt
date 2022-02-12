936c40d6b8dd39b9d6cc39a91d85f7307024c85d
ruslan <rus.mamedov.2003002@mail.ru>
2022-02-13 01:03:41 +0600

    hi

--- app/src/main/java/kg/example/homework61/EmailActivity.kt
+++ app/src/main/java/kg/example/homework61/EmailActivity.kt
@@ -8,17 +8,22 @@ import kg.example.homework61.databinding.ActivityEmailBinding
 class EmailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEmailBinding
 
+    companion object {
+        const val KEY1 = "edit_text"
+        const val KEY2 = "result"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEmailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.tvActivity.setText(intent.getStringExtra("edit_text"))
+        binding.tvActivity.setText(intent.getStringExtra(KEY1))
 
         binding.btnExit.setOnClickListener {
             setResult(
                 RESULT_OK, Intent().putExtra(
-                    "result", binding.tvActivity.text.toString()
+                    KEY2, binding.tvActivity.text.toString()
                 )
             )
             finish()
--- app/src/main/java/kg/example/homework61/MainActivity.kt
+++ app/src/main/java/kg/example/homework61/MainActivity.kt
@@ -13,26 +13,30 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var registerForActivityResult: ActivityResultLauncher<Intent>
 
+    companion object {
+        const val KEY1 = "edit_text"
+        const val KEY2 = "result"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnGo.setOnClickListener {
-            if (binding.tvMail.text.toString().isEmpty()){
-                Toast.makeText(this, "Вы нечего не написали!!!", Toast.LENGTH_SHORT).show()
-
-            }else{
-                Intent(this@MainActivity,EmailActivity::class.java).apply {
-                    putExtra("edit_text", binding.tvMail.text.toString())
+            if (binding.tvMail.text.toString().isEmpty()) {
+                Toast.makeText(this, getString(R.string.Noo), Toast.LENGTH_SHORT).show()
+            } else {
+                Intent(this@MainActivity, EmailActivity::class.java).apply {
+                    putExtra(KEY1, binding.tvMail.text.toString())
                     registerForActivityResult.launch(this)
                 }
             }
         }
         registerForActivityResult = registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
-        ){result->
-            binding.tvMail.setText(result.data?.getStringExtra("result"))
+        ) { result ->
+            binding.tvMail.setText(result.data?.getStringExtra(KEY2))
         }
     }
 }
